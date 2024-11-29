afadb3b21cbb940d276cde98cfee5c0039ecbc67
vinh doan <vinh.doan@blueoc.tech>
2024-11-29 14:59:55 +0700

    fix: comments the demo fetching

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,18 @@ import { Provider } from "react-redux";
 import store from "./redux/store";
 import Todo from "./components/Todo";
 import { Toaster } from "./components/ui/sonner";
-import { useEffect } from "react";
 
 function App() {
-  const handleFetchTodo = async () => {
-    const response = await fetch("http://localhost:5173/todo.json").then(
-      (res) => res.json()
-    );
+  // const handleFetchTodo = async () => {
+  //   const response = await fetch("http://localhost:5173/todo.json").then(
+  //     (res) => res.json()
+  //   );
 
-    console.log("response", response);
-  };
-  useEffect(() => {
-    handleFetchTodo();
-  }, []);
+  //   console.log("response", response);
+  // };
+  // useEffect(() => {
+  //   handleFetchTodo();
+  // }, []);
   return (
     <Provider store={store}>
       <Toaster />
